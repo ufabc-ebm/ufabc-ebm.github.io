c79f51e7503f55ed4df1b53f7da7deaa45d34c34
Erick <43915520+edlbcamargo@users.noreply.github.com>
2021-09-24 20:13:47 -0300

    Atualizando páginas: inicial, eng. clínica e instrumentação.

---
+++ footer.js
@@ -0,0 +1,71 @@
+document.write('\
+    <footer>\
+      <!-- Contact Us \
+      <div class="background-primary padding text-center">\
+        <p class="h1">Contact Us: 0800 787 787</p>                                                                        \
+      </div>-->\
+      \
+      <!-- Main Footer -->\
+      <section class="background-dark full-width">\
+          <!-- Map -->\
+        <div class="s-12 m-12 l-6 margin-m-bottom-2x">\
+          <div class="s-12 grayscale center">  	  \
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29235.287354484746!2d-46.56352249104412!3d-23.66125861652414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf1a53d9732f7a8c6!2sUniversidade+Federal+do+ABC%2C+Campus+S%C3%A3o+Bernardo+do+Campo!5e0!3m2!1spt-BR!2sbr!4v1545338512513" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>\
+          </div>\
+        </div>\
+        \
+        <!-- Collumn 2 -->\
+        <div class="s-12 m-12 l-6 margin-m-bottom-2x">\
+          <div class="padding-2x">\
+            <div class="line">              \
+              <div class="float-left">\
+                  <i class="icon-sli-location-pin text-primary icon3x"></i>\
+                </div>\
+                <div class="margin-left-70 margin-bottom-30">\
+                  <h3 class="margin-bottom-0">Engenharia Biomédica</h3>\
+                  <p><br>\
+                     \
+			Universidade Federal do ABC<br>\
+			Rua Arcturus, 03 - Jardim Antares -<br>\
+			São Bernardo do Campo - São Paulo<br>\
+			09606-070<br>\
+                  </p>               \
+                </div>\
+                <div class="float-left">\
+                  <i class="icon-sli-envelope text-primary icon3x"></i>\
+                </div>\
+                <div class="margin-left-70 margin-bottom-30">\
+                  <h3 class="margin-bottom-0">E-mail</h3>\
+                  <p>EBM Graduação: ebmUFABC@gmail.com <br>\
+		EBM Pós-Graduação: ppgebm@ufabc.edu.br\
+                  </p>              \
+                </div>\
+                <div class="float-left">\
+                  <i class="icon-sli-phone text-primary icon3x"></i>\
+                </div>\
+                <div class="margin-left-70">\
+                  <h3 class="margin-bottom-0">Phone Numbers</h3>\
+                  <p>EBM Graduação: +55 (11) 2320-6397/6396<br>\
+		     EBM Pós-Graduação: + 55 (11) 2320 - 6154/6157\
+                  </p>             \
+                </div>\
+            </div>\
+          </div>\
+        </div>  \
+      </section>\
+      <hr class="break margin-top-bottom-0" style="border-color: rgba(0, 38, 51, 0.80);">\
+      \
+      <!-- Bottom Footer -->\
+      <section class="padding background-dark full-width">\
+        <div class="s-12 l-6">\
+          <p class="text-size-12">Copyright 2021, Engenharia Biomédica</p>\
+        </div>\
+        <div class="s-12 l-6">\
+          <a class="right text-size-12" href="http://www.myresponsee.com" title="Responsee - lightweight responsive framework">Design and coding<br> by Responsee Team</a>\
+        </div>\
+      </section>\
+    </footer>\
+ 		<script type="text/javascript" src="js/responsee.js"></script>\
+		<script type="text/javascript" src="owl-carousel/owl.carousel.js"></script>\
+		<script type="text/javascript" src="js/template-scripts.js"></script>\
+');
---
+++ menu.js
@@ -0,0 +1,68 @@
+document.write('\
+     <header role="banner" class="position-absolute">    \
+      <!-- Top Navigation -->\
+      <nav class="background-transparent background-transparent-hightlight full-width sticky">\
+        <div class="s-2 l-2">\
+          <a href="index.html" class="logo">\
+            <!-- Logo White Version -->\
+            <img class="logo-white" src="img/ufabc-2021_high.png" alt="">\
+            <!-- Logo Dark Version -->\
+            <img class="logo-dark" src="img/ufabc-2021_high.png" alt="">\
+          </a>\
+        </div>\
+        <div class="top-nav s-10 l-10">\
+          <p class="nav-text"></p>\
+          <ul class="right chevron">\
+            <li><a href="index.html">Início</a></li>\
+            <li><a href="curso.html">Sobre o curso</a></li>\
+			<li><a href="Biomateriais.html">Biomateriais</a>\
+				<ul>\
+					<li><a href="Biomateriais.html">Biomateriais e dispositivos implantáveis</a></li>\
+					<li><a href="Biomateriais.html#Avancos">Avanços tecnológicos na área</a></li>\
+					<li><a href="Biomateriais.html#MedRegen">Medicina Regenerativa</a> </li>\
+					<li><a href="Biomateriais.html#EngTecidual">Engenharia Tecidual</a> </li>\
+				</ul>\
+			</li>\
+			<li><a href="Biomecanica.html">Biomecânica</a>\
+				<ul><li><a href="Biomecanica.html">Biomecânica e Controle Motor</a></li>\
+					<li><a href="Biomecanica.html#LabBiomec">Laboratório de Biomecânica e Controle Motor</a></li>\
+					<li><a href="Biomecanica.html#MovCorpo">Medida do movimento do corpo</a></li>\
+					<li><a href="Biomecanica.html#RestAnd">Restauração do Andar</a></li>\
+					<li><a href="Biomecanica.html#ControlMotor">Controle Motor </a></li>\
+				</ul>\
+			</li>\
+			<li><a href="EngenhariaClinica.html">Eng. Cl&iacute;nica</a>\
+				<ul>\
+					<li><a href="EngenhariaClinica.html#oqueehengclin">O que é Engenharia Clínica?</a></li>\
+					<li><a href="EngenhariaClinica.html#biomat">Biomateriais e dispositivos implantáveis</a></li>\
+					<li><a href="EngenhariaClinica.html#Avancos">Avanços tecnológicos na área</a></li>\
+					<li><a href="EngenhariaClinica.html#MedRegen">Medicina Regenerativa</a> </li>\
+					<li><a href="EngenhariaClinica.html#EngTecidual">Engenharia Tecidual</a> </li>\
+				</ul>\
+			</li>\
+            <li><a href="Instrumentacao.html">Instrumentação</a>\
+				<ul><li><a href="Instrumentacao.html#oqueehinstrumentacao">O que é Instrumentação Biomédica? </a></li>\
+					<li><a href="Instrumentacao.html#InstruUS">Ultrassom Clínico</a></li>\
+					<li><a href="Instrumentacao.html#InstruMRI">Instrumentação de Radio-Frequência para Imagens de Ressonância Magnética (MRI) </a></li>\
+					<li><a href="Instrumentacao.html#InstruI3D">Impressão 3D para planejamento cirúrgico   </a> </li>\
+					<li><a href="Instrumentacao.html#InstruTIE">Tomografia de Impedância Elétrica </a>  </li>\
+					<li><a href="Instrumentacao.html#InstruBiofonica">O que é Biofotônica e Óptica Biomédica?</a></li>\
+					<li><a href="Instrumentacao.html#Diagnostico">Diagnóstico usando Luz</a></li>\
+					<li><a href="Instrumentacao.html#Tratamento">Tratamentos usando Luz</a> </li>\
+				</ul>\
+			</li>\
+            <li><a href="Computacionais.html">Sist. Computacionais</a>\
+				<ul>\
+					<li><a href="Computacionais.html">Bioestatística Aplicada à Saúde e Informática Médica</a></li>\
+					<li><a href="Computacionais.html#BHIG">Bioinformatics and Health Informatics Group</a></li>\
+					<li><a href="Computacionais.html#Prostata">Relação genética para câncer de próstata</a></li>\
+					<li><a href="Computacionais.html#SinaisBiologicos">Processamento de Sinais Biológicas</a></a></li>\
+					<li><a href="Computacionais.html#ProtSup">Próteses Mioelétricas de Membro Superior</a></a></li>\
+					<li><a href="Computacionais.html#BSPM">Sistemas de Diagnósticos para Doenças do Coração</a> </li>\
+				</ul> \
+			</li>\
+          </ul>\
+        </div>\
+      </nav>\
+    </header>			\
+');
