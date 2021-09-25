081389fd80ee53be33e3719bb199b5764cf0e10d
Erick <43915520+edlbcamargo@users.noreply.github.com>
2021-09-25 13:41:24 -0300

    Atualização dos menus, e páginas biomateriais, biomecânica e computacionais.

--- menu.js
+++ menu.js
@@ -16,58 +16,64 @@ document.write('\
             <li><a href="index.html">Início</a></li>\
             <li><a href="curso.html">Sobre o curso</a>\
 				<ul>\
-					<li><a href="curso.html#ingresso">Ingresso no curso</a></li>\
-					<li><a href="curso.html#BCT">Bacharelado em Ciências e Tecnologia (BC&T)</a></li>\
+					<li><a href="curso.html#ingresso"       >Ingresso no curso</a></li>\
+					<li><a href="curso.html#BCT"            >Bacharelado em Ciências e Tecnologia (BC&T)</a></li>\
 					<li><a href="curso.html#InformacaoCurso">Informações  sobre o curso</a> </li>\
-					<li><a href="curso.html#CorpoDocente">O corpo docente</a> </li>\
-					<li><a href="curso.html#Disc">Disciplinas especí­ficas do curso</a> </li>\
-					<li><a href="curso.html#Eixos">Eixos do curso</a> </li>\
+					<li><a href="curso.html#CorpoDocente"   >O corpo docente</a> </li>\
+					<li><a href="curso.html#Disc"           >Disciplinas do curso</a> </li>\
+					<li><a href="curso.html#Eixos"          >Eixos do curso</a> </li>\
 				</ul>\
             </li>\
 			<li><a href="Biomateriais.html">Biomateriais</a>\
 				<ul>\
-					<li><a href="Biomateriais.html">Biomateriais e dispositivos implantáveis</a></li>\
-					<li><a href="Biomateriais.html#Avancos">Avanços tecnológicos na área</a></li>\
-					<li><a href="Biomateriais.html#MedRegen">Medicina Regenerativa</a> </li>\
-					<li><a href="Biomateriais.html#EngTecidual">Engenharia Tecidual</a> </li>\
+					<li><a href="Biomateriais.html#Biomateriais"    >Biomateriais e dispositivos implantáveis</a></li>\
+					<li><a href="Biomateriais.html#Avancos"         >Avanços tecnológicos na área</a></li>\
+					<li><a href="Biomateriais.html#MedRegen"        >Medicina Regenerativa</a> </li>\
+					<li><a href="Biomateriais.html#EngTecidual"     >Engenharia Tecidual</a> </li>\
+					<li><a href="Biomateriais.html#AvalBiomateriais">Avaliação Biológica de Biomateriais</a> </li>\
 				</ul>\
 			</li>\
 			<li><a href="Biomecanica.html">Biomecânica</a>\
-				<ul><li><a href="Biomecanica.html">Biomecânica e Controle Motor</a></li>\
-					<li><a href="Biomecanica.html#LabBiomec">Laboratório de Biomecânica e Controle Motor</a></li>\
-					<li><a href="Biomecanica.html#MovCorpo">Medida do movimento do corpo</a></li>\
-					<li><a href="Biomecanica.html#RestAnd">Restauração do Andar</a></li>\
+				<ul>\
+					<li><a href="Biomecanica.html"             >Biomecânica e Controle Motor</a></li>\
+					<li><a href="Biomecanica.html#LabBiomec"   >Laboratório de Biomecânica e Controle Motor</a></li>\
+					<li><a href="Biomecanica.html#MovCorpo"    >Medida do movimento do corpo</a></li>\
+					<li><a href="Biomecanica.html#RestAnd"     >Restauração do Andar</a></li>\
 					<li><a href="Biomecanica.html#ControlMotor">Controle Motor </a></li>\
 				</ul>\
 			</li>\
 			<li><a href="EngenhariaClinica.html">Eng. Cl&iacute;nica</a>\
 				<ul>\
 					<li><a href="EngenhariaClinica.html#oqueehengclin">O que é Engenharia Clínica?</a></li>\
-					<li><a href="EngenhariaClinica.html#biomat">Biomateriais e dispositivos implantáveis</a></li>\
-					<li><a href="EngenhariaClinica.html#Avancos">Avanços tecnológicos na área</a></li>\
-					<li><a href="EngenhariaClinica.html#MedRegen">Medicina Regenerativa</a> </li>\
-					<li><a href="EngenhariaClinica.html#EngTecidual">Engenharia Tecidual</a> </li>\
+					<li><a href="EngenhariaClinica.html#biomat"       >Biomateriais e dispositivos implantáveis</a></li>\
+					<li><a href="EngenhariaClinica.html#Avancos"      >Avanços tecnológicos na área</a></li>\
+					<li><a href="EngenhariaClinica.html#MedRegen"     >Medicina Regenerativa</a> </li>\
+					<li><a href="EngenhariaClinica.html#EngTecidual"  >Engenharia Tecidual</a> </li>\
 				</ul>\
 			</li>\
             <li><a href="Instrumentacao.html">Instrumentação</a>\
-				<ul><li><a href="Instrumentacao.html#oqueehinstrumentacao">O que é Instrumentação Biomédica? </a></li>\
-					<li><a href="Instrumentacao.html#InstruUS">Ultrassom Clínico</a></li>\
-					<li><a href="Instrumentacao.html#InstruMRI">Instrumentação de Radio-Frequência para Imagens de Ressonância Magnética (MRI) </a></li>\
-					<li><a href="Instrumentacao.html#InstruI3D">Impressão 3D para planejamento cirúrgico   </a> </li>\
-					<li><a href="Instrumentacao.html#InstruTIE">Tomografia de Impedância Elétrica </a>  </li>\
-					<li><a href="Instrumentacao.html#InstruBiofonica">O que é Biofotônica e Óptica Biomédica?</a></li>\
-					<li><a href="Instrumentacao.html#Diagnostico">Diagnóstico usando Luz</a></li>\
-					<li><a href="Instrumentacao.html#Tratamento">Tratamentos usando Luz</a> </li>\
+				<ul>\
+					<li><a href="Instrumentacao.html#oqueehinstrumentacao">O que é Instrumentação Biomédica? </a></li>\
+					<li><a href="Instrumentacao.html#InstruUS"            >Ultrassom Clínico</a></li>\
+					<li><a href="Instrumentacao.html#InstruMRI"           >Instrumentação de Radio-Frequência para Imagens de Ressonância Magnética (MRI) </a></li>\
+					<li><a href="Instrumentacao.html#InstruI3D"           >Impressão 3D para planejamento cirúrgico   </a> </li>\
+					<li><a href="Instrumentacao.html#InstruTIE"           >Tomografia de Impedância Elétrica </a>  </li>\
+					<li><a href="Instrumentacao.html#InstruBiofonica"     >O que é Biofotônica e Óptica Biomédica?</a></li>\
+					<li><a href="Instrumentacao.html#Diagnostico"         >Diagnóstico usando Luz</a></li>\
+					<li><a href="Instrumentacao.html#Tratamento"          >Tratamentos usando Luz</a> </li>\
 				</ul>\
 			</li>\
             <li><a href="Computacionais.html">Sist. Computacionais</a>\
 				<ul>\
-					<li><a href="Computacionais.html">Bioestatística Aplicada à Saúde e Informática Médica</a></li>\
-					<li><a href="Computacionais.html#BHIG">Bioinformatics and Health Informatics Group</a></li>\
-					<li><a href="Computacionais.html#Prostata">Relação genética para câncer de próstata</a></li>\
-					<li><a href="Computacionais.html#SinaisBiologicos">Processamento de Sinais Biológicas</a></a></li>\
-					<li><a href="Computacionais.html#ProtSup">Próteses Mioelétricas de Membro Superior</a></a></li>\
-					<li><a href="Computacionais.html#BSPM">Sistemas de Diagnósticos para Doenças do Coração</a> </li>\
+					<li><a href="Computacionais.html#Atuacao"         >O que são sistemas computacionais aplicados a ciências da vida?</a></li>\
+					<li><a href="Computacionais.html#Disciplinas"     >Disciplinas do eixo</a></li>\
+					<li><a href="Computacionais.html#BASeIM"          >Bioinformática Aplicada à Saúde e Informática Médica</a></li>\
+					<li><a href="Computacionais.html#BHIG"            >Bioinformatics and Health Informatics Group</a></li>\
+					<li><a href="Computacionais.html#Prostata"        >Relação genética para câncer de próstata</a></li>\
+					<li><a href="Computacionais.html#SinaisBiologicos">O que é Processamento de Sinais Biológicos?</a></li>\
+					<li><a href="Computacionais.html#ProtSup"         >Próteses Mioelétricas de Membro Superior</a></li>\
+					<li><a href="Computacionais.html#BSPM"            >Sistemas de Diagnósticos para Doenças do Coração</a> </li>\
+					<li><a href="Computacionais.html#Modelagem"       >Modelagem</a></li>\
 				</ul> \
 			</li>\
           </ul>\
